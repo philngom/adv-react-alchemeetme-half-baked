b231b2f9851d9f4a38596d233dea1c8664813c25
Philippe Ngom <philngom@gmail.com>
2022-04-25 15:17:35 -0700

    wrote test for header. Test passing.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,19 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(
+    <App />
+  )
 
-})
+  return waitFor(() => {
+    screen.findByText('Meet Vonta');
+  })
+});
